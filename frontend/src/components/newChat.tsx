a9d0361ebf71ff111df24135caa78249f5ae48ea
aaron <ajwills@umich.edu>
2026-02-10 21:24:51 -0500

    making UI pretty

--- frontend/src/components/newChat.tsx
+++ frontend/src/components/newChat.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
+
+import { Pencil } from "lucide-react";
+
 type newChatType = {
   newChat: () => void;
 };
 
 const NewChat = ({ newChat }: newChatType) => {
-  return <button onClick={() => newChat()}> newChat </button>;
+  return (
+    <Button className="ml-auto" variant="outline" onClick={() => newChat()}>
+      <Pencil />
+    </Button>
+  );
 };
 
 export default NewChat;
