6a5a4d0670eb227f9da77f861808387537da9067
aaron <ajwills@umich.edu>
2026-02-13 15:47:26 -0500

    bare bones sidebar working

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,23 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import Session from "./components/session.tsx";
+import Session from "./pages/session.tsx";
 import ConvoList from "./components/convoList.tsx";
 import { ConvoProvider } from "./context/convoContext.tsx";
+import { SidebarProvider } from "./components/ui/sidebar.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ConvoProvider>
       <BrowserRouter>
-        <div className="h-screen flex flex-row">
+        <SidebarProvider>
           <div className="w-fit m-10">
             <ConvoList />
           </div>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/chat/:id" element={<Session />} />
-          </Routes>
-        </div>
+          <div className="flex-1 flex h-svh">
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/chat/:id" element={<Session />} />
+            </Routes>
+          </div>
+        </SidebarProvider>
       </BrowserRouter>
     </ConvoProvider>
   </StrictMode>,
