b6070cffd49853334cf67618e086604d1fba9ad2
aaron <ajwills@umich.edu>
2026-02-13 12:15:53 -0500

    got rid of stuff commented out

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,9 @@
 import ConvoList from "./components/convoList";
 import Session from "./components/session";
-import { useState } from "react";
-import services from "./services/index";
 import HomeInput from "./components/homeInput";
 import { useConvo } from "./context/convoContext";
 
 function App() {
-  //websocket wikll automatically send back new message, update all messages
-
-  // const [currentView, setCurrentView] = useState<"newChat" | "chat">("newChat");
-  // const [convoId, setConvoId] = useState<string>("");
-  // const [newMessage, setNewMessage] = useState("");
-
-  // const selectConvo = (id: string) => {
-  //   setConvoId(id);
-  //   setCurrentView("chat");
-  // };
-
-  // const handleMsgChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-  //   setNewMessage(event.target.value);
-  //   console.log(event.target.value);
-  // };
-
-  // const sendMessage = async (id: string) => {
-  //   console.log("message sent", newMessage);
-  //   setNewMessage("");
-  //   await services.sendMessage({
-  //     content: newMessage,
-  //     role: "user",
-  //     convoId: id,
-  //   });
-  // };
-
-  // const createConversation = async () => {
-  //   console.log("message sent", newMessage);
-  //   const res = await services.createConversation({
-  //     content: newMessage,
-  //   });
-  //   setConvoId(res.convoId);
-  //   setCurrentView("chat");
-  //   setNewMessage("");
-  // };
-
-  // const newChat = async () => {
-  //   setCurrentView("newChat");
-  //   console.log("newChatClicked");
-  // };
-  //
-
   const convo = useConvo();
 
   if (!convo) throw new Error("useConvo not working");
